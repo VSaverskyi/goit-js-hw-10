4685578e1fed7714adb06ecfd8cd32595200cb30
VSaverskyi <vovikck15@gmail.com>
2023-03-21 21:45:40 +0100

    add function fetch-countries

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './css/styles.css';
+import fetchCountries from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  searchForm: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
+};
+
+refs.searchForm.addEventListener('input', deboncedOnSearch);
