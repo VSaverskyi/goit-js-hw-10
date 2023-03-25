9fdff222709f07e60cfedc7eee5cd64af2303837
VSaverskyi <vovikck15@gmail.com>
2023-03-25 19:48:55 +0100

    add full functional

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import debounce from 'lodash.debounce';
 import './css/styles.css';
 import { FetchCountriesAPI } from './fetchCountries';
+import createCountriesList from './templates/create-countries-list.hbs';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const countriesSearchInputEl = document.querySelector('#search-box');
 const countryListEl = document.querySelector('.country-list');
@@ -14,16 +16,51 @@ const onCountriesSearchInput = e => {
   const seekedCountry = e.target.value.trim();
 
   if (seekedCountry === '') {
-    clearCountriesInfo();
+    clearCountryInfo();
+    clearCountriesList();
+    return;
   }
 
   fetchCountriesAPI
     .fetchCountries(seekedCountry)
     .then(data => {
-      countryInfoEl.innerHTML = `<div class="flags-wrapper"><img src=${
-        data[0].flags.svg
-      } alt=${data[0].flags.alt} width="60" height="40">
-<h2 class="country-name">${data[0].name.official}</h2></div>
+      if (data.length === 1) {
+        clearCountriesList();
+        markUpCountryCard(data);
+      } else if (data.length >= 2 && data.length <= 10) {
+        clearCountryInfo();
+        markUpCountriesList(data);
+      } else {
+        Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      }
+    })
+    .catch(err => {
+      clearCountriesList();
+      clearCountryInfo();
+      Notify.failure('Oops, there is no country with that name');
+    });
+};
+
+countriesSearchInputEl.addEventListener(
+  'input',
+  debounce(onCountriesSearchInput, DEBOUNCE_DELAY)
+);
+
+function clearCountryInfo() {
+  countryInfoEl.innerHTML = '';
+}
+
+function clearCountriesList() {
+  countryListEl.innerHTML = '';
+}
+
+function markUpCountryCard(data) {
+  countryInfoEl.innerHTML = `<div class="flags-wrapper">
+  <img src=${data[0].flags.svg} alt=${data[0].flags.alt} width="40">
+<h2 class="country-name">${data[0].name.official}</h2>
+</div>
 <ul class="country-info list">
     <li class="country-info-item">
         <p class="capital">Capital: ${data[0].capital}</p>
@@ -35,21 +72,8 @@ const onCountriesSearchInput = e => {
         <p class="languages">Languages: ${Object.values(data[0].languages)}</p>
     </li>
 </ul>`;
-      console.log(data);
-    })
-    .catch(err => {
-      console.log(err);
-      countryInfoEl.innerHTML = 'Oops, there is no country with that name';
-    });
-};
+}
 
-countriesSearchInputEl.addEventListener(
-  'input',
-  debounce(onCountriesSearchInput, DEBOUNCE_DELAY)
-);
-
-function clearCountriesInfo() {
-  countryInfoEl.innerHTML = '';
-  countryListEl.innerHTML = '';
-  return;
+function markUpCountriesList(data) {
+  countryListEl.innerHTML = createCountriesList(data);
 }
