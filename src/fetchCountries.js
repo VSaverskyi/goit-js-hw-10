b5440df00129a885ab8b5f378c9c9c040cab6f9b
VSaverskyi <vovikck15@gmail.com>
2023-03-25 18:31:35 +0100

    add first style

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,12 +1,18 @@
-const BASE_URL = 'https://restcountries.com/v3.1';
+'use strict';
 
-export function fetchCountries(name) {
-  return fetch(
-    `${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`
-  ).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
+export class FetchCountriesAPI {
+  #BASE_URL = 'https://restcountries.com/v3.1';
+
+  fetchCountries(name) {
+    return fetch(
+      `${
+        this.#BASE_URL
+      }/name/${name}?fields=name,capital,population,flags,languages`
+    ).then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    });
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,45 +1,55 @@
 import debounce from 'lodash.debounce';
 import './css/styles.css';
-import { fetchCountries } from './fetchCountries';
+import { FetchCountriesAPI } from './fetchCountries';
+
+const countriesSearchInputEl = document.querySelector('#search-box');
+const countryListEl = document.querySelector('.country-list');
+const countryInfoEl = document.querySelector('.country-info');
 
 const DEBOUNCE_DELAY = 300;
 
-const refs = {
-  countriesSearchInput: document.querySelector('#search-box'),
-  countryList: document.querySelector('.country-list'),
-  countryInfo: document.querySelector('.country-info'),
+const fetchCountriesAPI = new FetchCountriesAPI();
+
+const onCountriesSearchInput = e => {
+  const seekedCountry = e.target.value.trim();
+
+  if (seekedCountry === '') {
+    clearCountriesInfo();
+  }
+
+  fetchCountriesAPI
+    .fetchCountries(seekedCountry)
+    .then(data => {
+      countryInfoEl.innerHTML = `<div class="flags-wrapper"><img src=${
+        data[0].flags.svg
+      } alt=${data[0].flags.alt} width="60" height="40">
+<h2 class="country-name">${data[0].name.official}</h2></div>
+<ul class="country-info list">
+    <li class="country-info-item">
+        <p class="capital">Capital: ${data[0].capital}</p>
+    </li>
+    <li class="country-info-item">
+        <p class="population">Population: ${data[0].population}</p>
+    </li>
+    <li class="country-info-item">
+        <p class="languages">Languages: ${Object.values(data[0].languages)}</p>
+    </li>
+</ul>`;
+      console.log(data);
+    })
+    .catch(err => {
+      console.log(err);
+      countryInfoEl.innerHTML = 'Oops, there is no country with that name';
+    });
 };
 
-refs.countriesSearchInput.addEventListener(
+countriesSearchInputEl.addEventListener(
   'input',
   debounce(onCountriesSearchInput, DEBOUNCE_DELAY)
 );
 
-function onCountriesSearchInput(e) {
-  const seekedCountry = e.target.value.trim();
-  console.log(seekedCountry);
-  fetchCountries(seekedCountry)
-    .then(data => {
-      console.log(data[0].flags.svg);
-      refs.countryInfo.innerHTML = `<img src=${data[0].flags.svg} alt=${
-        data[0].flags.alt
-      } width="60">
-      <h2 class="country-name">${data[0].name.official}</h2>
-      <ul class="country-info list">
-          <li class="country-info-item">
-              <p class="capital">Capital: ${data[0].capital}</p>
-          </li>
-          <li class="country-info-item">
-              <p class="population">Population: ${data[0].population}</p>
-          </li>
-          <li class="country-info-item">
-              <p class="languages">Languages: ${Object.values(
-                data[0].languages
-              )}</p>
-          </li>
-      </ul>`;
-    })
-    .catch(err => {
-      refs.countryInfo.innerHTML = 'Oops, there is no country with that name';
-    });
+function clearCountriesInfo() {
+  countryInfoEl.innerHTML = '';
+  countryListEl.innerHTML = '';
+  return;
 }
